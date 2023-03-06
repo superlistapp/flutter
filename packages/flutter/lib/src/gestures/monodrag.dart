cdb624f9457bb6b1b508b34bdada01e17f9ef9e9
Matej Knopp <matej.knopp@gmail.com>
2023-03-06 14:37:44 +0100

    Fix clicking on magic mouse while pan is in progress

--- packages/flutter/lib/src/gestures/monodrag.dart
+++ packages/flutter/lib/src/gestures/monodrag.dart
@@ -298,6 +298,7 @@ abstract class DragGestureRecognizer extends OneSequenceGestureRecognizer {
   // The buttons sent by `PointerDownEvent`. If a `PointerMoveEvent` comes with a
   // different set of buttons, the gesture is canceled.
   int? _initialButtons;
+  int? _initialPointer;
   Matrix4? _lastTransform;
 
   /// Distance moved in the global coordinate space of the screen in drag direction.
@@ -343,6 +344,11 @@ abstract class DragGestureRecognizer extends OneSequenceGestureRecognizer {
         return false;
       }
     }
+    if (_initialPointer != null &&
+        event.kind == PointerDeviceKind.mouse &&
+        event.pointer != _initialPointer) {
+      return false;
+    }
     return super.isPointerAllowed(event as PointerDownEvent);
   }
 
@@ -366,6 +372,7 @@ abstract class DragGestureRecognizer extends OneSequenceGestureRecognizer {
     super.addAllowedPointer(event);
     if (_state == _DragState.ready) {
       _initialButtons = event.buttons;
+      _initialPointer = event.pointer;
     }
     _addPointer(event);
   }
@@ -376,6 +383,7 @@ abstract class DragGestureRecognizer extends OneSequenceGestureRecognizer {
     startTrackingPointer(event.pointer, event.transform);
     if (_state == _DragState.ready) {
       _initialButtons = kPrimaryButton;
+      _initialPointer = event.pointer;
     }
     _addPointer(event);
   }
@@ -485,6 +493,7 @@ abstract class DragGestureRecognizer extends OneSequenceGestureRecognizer {
     _hasDragThresholdBeenMet = false;
     _velocityTrackers.clear();
     _initialButtons = null;
+    _initialPointer = null;
     _state = _DragState.ready;
   }
 
